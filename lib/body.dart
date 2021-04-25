e941a8b666db97c28827009d69fc30d9d3d13418
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-04-25 12:52:43 +0600

    body

--- lib/body.dart
+++ lib/body.dart
@@ -5,13 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'components/indicator_dot.dart';
-import 'paints/wheel_svg.dart';
 import 'wheel_handler.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
+
+///lets' make it for phone 1st
 
 class _HomeScreenState extends State<HomeScreen> {
   final double _dotRadius = 25.0;
@@ -22,49 +23,60 @@ class _HomeScreenState extends State<HomeScreen> {
       fontSize: Theme.of(context).textTheme.headline4!.fontSize,
       fontWeight: FontWeight.w800,
     );
-    return LayoutBuilder(
-      builder: (context, constraints) => Stack(
-        children: [
-          Positioned(
-            left: 10,
-            top: 100,
-            child: Header(
-              "Enter".toUpperCase(),
-              _headerTextStyle,
+    return Container(
+      color: Colors.yellow.withOpacity(.5),
+      child: LayoutBuilder(builder: (context, constraints) {
+        double _containerWidth = constraints.maxWidth * .9;
+        double _wheelSize = _containerWidth * .835;
+
+        return Stack(
+          children: [
+            Positioned(
+              left: _containerWidth * 0.1,
+              top: _containerWidth * 0.2,
+              child: Header(
+                "Enter".toUpperCase(),
+                _headerTextStyle,
+              ),
             ),
-          ),
-          Positioned(
-            left: 10,
-            top: 130,
-            child: Header(
-              "Passcode".toUpperCase(),
-              _headerTextStyle,
+            Positioned(
+              left: _containerWidth * 0.1,
+              top: _containerWidth * 0.27,
+              child: Header(
+                "Passcode".toUpperCase(),
+                _headerTextStyle,
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment(.8, -.5),
-            child: buildDots(),
-          ),
-          Align(
-            alignment: Alignment(0, .3),
-            child: Container(
-              height: constraints.maxHeight * .5,
-              width: constraints.maxHeight * .5,
-              child: CustomPaint(
-                isComplex: true,
-                painter: SpringHousePaint(
-                  dialerRadius: constraints.maxHeight * .2,
+            Align(
+              alignment: Alignment(.8, -.5),
+              child: buildDots(),
+            ),
+            Align(
+              alignment: Alignment(0, .1),
+              child: Container(
+                height: _containerWidth,
+                width: _containerWidth,
+                child: CustomPaint(
+                  isComplex: true,
+                  painter: SpringHousePaint(
+                    dialerRadius: _containerWidth * .45,
+                  ),
                 ),
               ),
             ),
-          ),
-          Align(
-              alignment: Alignment(-0.1, .3),
-              child: WheelStateWidget(
-                constraints: constraints,
-              ))
-        ],
-      ),
+            Align(
+                alignment: Alignment(0, .1),
+                /// this container helps to align with `SpringHousePaint`
+                child: Container(
+                  height: _containerWidth,
+                  width: _containerWidth,
+                  child: WheelStateWidget(
+                    size: _wheelSize,
+                  ),
+                ))
+          ],
+        );
+      }),
     );
   }
 
