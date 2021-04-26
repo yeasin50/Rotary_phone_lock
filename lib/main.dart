16b4eba8c3618c7aabbfb47cfa7b491691b91c53
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-04-26 17:19:29 +0600

    AddedMulti-Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:dial_lock/provider/dot_provider.dart';
+import 'package:provider/provider.dart';
+
 import 'body.dart';
 import 'package:flutter/material.dart';
 
@@ -8,15 +11,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: SafeArea(
-          child: HomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => DotProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: SafeArea(
+            child: HomeScreen(),
+          ),
         ),
       ),
     );
